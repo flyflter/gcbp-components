b0210fbc24dc49985079f591fba523653bb1f065
zhongy-g <zhongy-g@glodon.com>
2022-03-20 18:17:49 +0800

    组件库初探

---
+++ packages/web-components/examples/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <GlButton type="primary">12345</GlButton>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+</style>
